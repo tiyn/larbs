fbf0eab195b33248014537dda804b847275db1ec
TiynGER <43725244+TiynGER@users.noreply.github.com>
2019-10-01 17:06:40 +0200

    0.0.9 preparation for switching dotfiles

--- larbs.sh
+++ larbs.sh
@@ -199,12 +199,6 @@ manualinstall $aurhelper || error "Failed to install AUR helper."
 # and all build dependencies are installed.
 installationloop
 
-# Install the dotfiles in the user's home directory
-echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
-echo ".cfg" >> $HOME/.gitignore
-git clone --bare "$dotfilesrepo" $HOME/.dotfiles
-rm -f "/home/$name/README.md" "/home/$name/LICENSE"
-
 # Install the LARBS Firefox profile in ~/.mozilla/firefox/
 putgitrepo "https://github.com/LukeSmithxyz/mozillarbs.git" "/home/$name/.mozilla/firefox"
 
