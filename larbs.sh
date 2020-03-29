8b2f08dc84653829fe8887e58572aaeeb7f13271
Marten Kante <43725244+TiynGER@users.noreply.github.com>
2020-03-29 21:52:27 +0200

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -219,9 +219,6 @@ systembeepoff
 newperms "%wheel ALL=(ALL) ALL #LARBS
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
 
-# Remove unwanted files
-rm -r ~/.local/share/larb
-
 # Last message! Install complete!
 finalize
 clear
