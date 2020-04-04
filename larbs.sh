437cb705092502d448bde1bd56c47df62fc41fb3
Marten Kante <43725244+TiynGER@users.noreply.github.com>
2020-04-04 11:23:40 +0200

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -177,10 +177,7 @@ adduserandpass || error "Error adding username and/or password."
 refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
 dialog --title "LARBS Installation" --infobox "Installing \`basedevel\` and \`git\` for installing other software required for the installation of other programs." 5 70
-installpkg curl
-installpkg base-devel
-installpkg git
-installpkg ntp
+pacman --noconfirm --needed -S curl base-devel git ntp
 
 [ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
 
